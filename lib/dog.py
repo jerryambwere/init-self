ae47406dc7188e81dc0b27abc250df8e30233643
Melvin jerry <+>
2024-10-06 15:22:21 +0300

    push

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
 
 class Dog:
+    def __init__(self,name,breed="Mutt"):
+        self.breed=breed
+        self.name=name
     pass
+cindy=  Dog("Cindy","Shephered")
+print(cindy.breed)
+print(cindy.name)
